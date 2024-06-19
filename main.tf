ab9e3063a091c2ad6cb6a323bdb039be9b66f0a9
Khuthadzo Bele <bgancloudsec@gmail.com>
2024-06-19 20:21:40 +0000

    Terraform Random Bucket Name #9

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.54.1"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length   = 16
+  special  = false
+  upper = false
+  lower = true
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
